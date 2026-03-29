89863dc10b14a7b10527b43c4e5c640e8f10713c
Kauã Régis <kauarlc@hotmail.com>
2026-03-29 02:37:09 -0300

    chore: algoritmo pra criar superusuário no deploy

--- build.sh
+++ build.sh
@@ -5,3 +5,13 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(email='$DJANGO_SUPERUSER_EMAIL').exists():
+    User.objects.create_superuser(
+        name='$DJANGO_SUPERUSER_NAME',
+        email='$DJANGO_SUPERUSER_EMAIL',
+        password='$DJANGO_SUPERUSER_PASSWORD',
+    )
+"
